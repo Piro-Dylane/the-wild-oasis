5e9f363c25cf36b8c8137e5fcb7a883cd1bfdcd5
Dylane Piro <piro.dylan@outlook.fr>
2025-02-05 18:40:19 +0100

    added login stored in cache

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,94 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
-import { StyleSheetManager } from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+import { StyleSheetManager } from 'styled-components';
 
-import isPropValid from "@emotion/is-prop-valid";
-import GlobalStyles from "./styles/GlobalStyles.js";
-import AppLayout from "./ui/AppLayout.jsx";
+import isPropValid from '@emotion/is-prop-valid';
+import GlobalStyles from './styles/GlobalStyles.js';
+import AppLayout from './ui/AppLayout.jsx';
 
-import Dashboard from "./pages/Dashboard.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import Login from "./pages/Login.jsx";
-import Account from "./pages/Account.jsx";
-import Users from "./pages/Users.jsx";
-import Settings from "./pages/Settings.jsx";
-import Cabins from "./pages/Cabins.jsx";
-import Bookings from "./pages/Bookings.jsx";
-import Booking from "./pages/Booking.jsx";
-import Checkin from "./pages/Checkin.jsx";
+import Dashboard from './pages/Dashboard.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+import Login from './pages/Login.jsx';
+import Account from './pages/Account.jsx';
+import Users from './pages/Users.jsx';
+import Settings from './pages/Settings.jsx';
+import Cabins from './pages/Cabins.jsx';
+import Bookings from './pages/Bookings.jsx';
+import Booking from './pages/Booking.jsx';
+import Checkin from './pages/Checkin.jsx';
+import ProtectedRoute from './ui/ProtectedRoute.jsx';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // The amount of time the data will stay in the cache (1 min = 60sec * 1000ms)
-      // staleTime: 60 * 1000,
-      staleTime: 0,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			// The amount of time the data will stay in the cache (1 min = 60sec * 1000ms)
+			// staleTime: 60 * 1000,
+			staleTime: 0,
+		},
+	},
 });
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <StyleSheetManager
-        shouldForwardProp={(propName, elementToBeRendered) => {
-          return typeof elementToBeRendered === "string"
-            ? isPropValid(propName)
-            : true;
-        }}>
-        <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="checkin/:bookingId" element={<Checkin />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
-            </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+			<StyleSheetManager
+				shouldForwardProp={(propName, elementToBeRendered) => {
+					return typeof elementToBeRendered === 'string'
+						? isPropValid(propName)
+						: true;
+				}}
+			>
+				<GlobalStyles />
+				<BrowserRouter>
+					<Routes>
+						<Route
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}
+						>
+							<Route index element={<Navigate replace to="dashboard" />} />
+							<Route path="dashboard" element={<Dashboard />} />
+							<Route path="bookings" element={<Bookings />} />
+							<Route path="bookings/:bookingId" element={<Booking />} />
+							<Route path="checkin/:bookingId" element={<Checkin />} />
+							<Route path="cabins" element={<Cabins />} />
+							<Route path="users" element={<Users />} />
+							<Route path="settings" element={<Settings />} />
+							<Route path="account" element={<Account />} />
+						</Route>
+						<Route path="login" element={<Login />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
 
-        <Toaster
-          position="top-center"
-          gutter={12}
-          containerStyle={{ margin: "8px" }}
-          toastOptions={{
-            success: {
-              duration: 3000,
-            },
-            error: {
-              duration: 5000,
-            },
-            style: {
-              fontSize: "16px",
-              maxWidth: "500px",
-              padding: "16px 24px",
-              backgroundColor: "var(--color-grey-0)",
-              color: "var(--color-grey-700)",
-            },
-          }}
-        />
-      </StyleSheetManager>
-    </QueryClientProvider>
-  );
+				<Toaster
+					position="top-center"
+					gutter={12}
+					containerStyle={{ margin: '8px' }}
+					toastOptions={{
+						success: {
+							duration: 3000,
+						},
+						error: {
+							duration: 5000,
+						},
+						style: {
+							fontSize: '16px',
+							maxWidth: '500px',
+							padding: '16px 24px',
+							backgroundColor: 'var(--color-grey-0)',
+							color: 'var(--color-grey-700)',
+						},
+					}}
+				/>
+			</StyleSheetManager>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,22 +1,23 @@
-import { useMutation } from "@tanstack/react-query";
-import { login as loginApi } from "../../services/apiAuth";
-import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 export function useLogin() {
-  const navigate = useNavigate();
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
 
-  const { mutate: login, isPending } = useMutation({
-    mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: (user) => {
-      console.log(user);
-      navigate("/dashboard");
-    },
-    onError: (err) => {
-      console.log("ERROR", err);
-      toast.error("Provided email or password are incorrect");
-    },
-  });
+	const { mutate: login, isPending } = useMutation({
+		mutationFn: ({ email, password }) => loginApi({ email, password }),
+		onSuccess: (user) => {
+			queryClient.setQueriesData(['user', user]);
+			navigate('/dashboard');
+		},
+		onError: (err) => {
+			console.log('ERROR', err);
+			toast.error('Provided email or password are incorrect');
+		},
+	});
 
-  return { login, isPending };
+	return { login, isPending };
 }
