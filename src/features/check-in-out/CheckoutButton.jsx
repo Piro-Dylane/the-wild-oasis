07dab0d64673f998d57d44da970dfbb7bb222f07
Dylane Piro <piro.dylan@outlook.fr>
2025-02-07 13:27:32 +0100

    added dashboard

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,14 @@
 import Button from "../../ui/Button";
+import { useCheckOut } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckOut();
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      disabled={isCheckingOut}
+      onClick={() => checkout(bookingId)}>
       Check out
     </Button>
   );
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,9 @@ import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
 import Stats from "./Stats";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -32,6 +35,9 @@ function DashboardLayout() {
         confirmedStays={confirmedStays}
         cabinCount={cabins?.length}
       />
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
